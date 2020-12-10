1bcf1d523921f918203fb8fe2da94cd842f037f0
xaviz5 <xavier5-20_lenin@hotmail.com>
2020-12-10 09:44:55 -0500

    Modificacion clase Estudiante con atributo edad

--- src/main/java/com/clearminds/lco/dtos/Estudiante.java
+++ src/main/java/com/clearminds/lco/dtos/Estudiante.java
@@ -5,26 +5,29 @@ public class Estudiante {
 	private String nombre;
 	private String apellido;
 	private int id;
+	private int edad;
 	
 	public Estudiante() {
 	}
 	
-	public Estudiante(int id, String nombre, String apellido) {
+	public Estudiante(int id, String nombre, String apellido, int edad) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
 		this.id = id;
+		this.edad = edad;
 	}
 
-	public Estudiante(String nombre, String apellido) {
+	public Estudiante(String nombre, String apellido, int edad) {
 		super();
 		this.nombre = nombre;
 		this.apellido = apellido;
+		this.edad = edad;
 	}
 
 	@Override
 	public String toString() {
-		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + "]";
+		return "Estudiante [nombre=" + nombre + ", apellido=" + apellido + ", id=" + id + ", edad=" + edad + "]";
 	}
 
 	public String getNombre() {
@@ -49,6 +52,14 @@ public class Estudiante {
 
 	public void setId(int id) {
 		this.id = id;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
 	}
 
 }
